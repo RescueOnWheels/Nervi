de21b847f21002347bf1dd81ade812a37295bec3
Martijn Vegter <mail@mvegter.me>
2018-10-09 19:45:23 +0200

    More ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,46 @@
 module.exports = {
   "extends": "airbnb-base",
   "rules": {
-    "import/no-unresolved": ["error", { ignore: ['pigpio'] }]
+    "import/no-unresolved": ["error", { ignore: ['pigpio'] }],
+    "multiline-comment-style": ["error", "starred-block"],
+    "no-multiple-empty-lines": ["error", {
+      "max": 1,
+      "maxBOF": 0,
+      "maxEOF": 1
+    }],
+    "require-jsdoc": ["error", {
+      "require": {
+        "FunctionDeclaration": true,
+        "MethodDefinition": true,
+        "ClassDeclaration": true,
+        "ArrowFunctionExpression": true,
+        "FunctionExpression": true
+      }
+    }],
+    "spaced-comment": ["error", "always", {
+      "block": {
+        "balanced": true
+      }
+    }],
+    "valid-jsdoc": ["error", {
+      "prefer": {
+        "arg": "param",
+        "argument": "param",
+        "class": "class",
+        "return": "returns",
+        "virtual": "abstract"
+      },
+      "preferType": {
+        "boolean": "Boolean",
+        "number": "Number",
+        "object": "Object",
+        "string": "String"
+      },
+      "matchDescription": ".+",
+      "requireParamDescription": true,
+      "requireReturn": false,
+      "requireReturnDescription": true,
+      "requireReturnType": true,
+    }]
   }
 };
