32df5a109b250a4834bcef9b548a9ff4e5e10a1d
Martijn Vegter <mail@mvegter.me>
2018-10-19 18:36:06 +0200

    Never Run This Commit As Root. (#27)

---
+++ test/mock/rotary.test.js
@@ -0,0 +1,52 @@
+/* Packages */
+const chai = require('chai');
+
+/* Test target */
+const Rotary = require('./../../lib/rotary');
+
+chai.should();
+
+module.exports = () => {
+  let rotary;
+
+  // Arrange
+  beforeEach(() => {
+    rotary = new Rotary();
+  });
+
+  it('should send its data to the `speed` event.', (done) => {
+    // Assert
+    rotary.on('speed', () => {
+      clearInterval(rotary.interval);
+      done();
+    });
+  });
+
+  it('should send 0 m/s to the `speed` event, if there were no interrupts.', (done) => {
+    // Assert
+    rotary.on('speed', (speed) => {
+      clearInterval(rotary.interval);
+
+      speed.should.equal(0);
+      done();
+    });
+  });
+
+  it('should send 0 m/s to the `speed` event, if there were no interrupts.', (done) => {
+    // Assert
+    rotary.on('speed', (speed) => {
+      clearInterval(rotary.interval);
+
+      const expectedDistance = Math.round((65 / 1000) * Math.PI, 2);
+      const actualSpeed = Math.round(speed, 2);
+
+      actualSpeed.should.equal(expectedDistance);
+      done();
+    });
+
+    // Act
+    for (let i = 0; i < 10; i += 1) {
+      rotary.rotary.triggerEvent('interrupt');
+    }
+  });
+};
