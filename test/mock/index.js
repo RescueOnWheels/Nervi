d1b6074e1e10cf201131d8dce5f516873c01ceba
Martijn Vegter <mail@mvegter.me>
2018-11-04 22:03:35 +0100

    Added servo tests

--- test/mock/index.js
+++ test/mock/index.js
@@ -1,10 +1,12 @@
 const rotary = require('./rotary.test');
+const servo = require('./servo.test');
 const ultrasonic = require('./ultrasonic.test');
 
 const normalize = require('./helpers/normalize.test');
 
 describe('Mock', () => {
   describe('rotary', rotary);
+  describe('servo', servo);
   describe('ultrasonic', ultrasonic);
 
   describe('helpers', () => {
