687286b342feb197f1e4e3d14d8dcf21f1e4801b
Martijn Vegter <mail@mvegter.me>
2018-11-14 09:19:11 +0100

    Integrated Auxilio

--- test/mock/index.js
+++ test/mock/index.js
@@ -4,16 +4,10 @@ const rotary = require('./rotary.test');
 const servo = require('./servo.test');
 const ultrasonic = require('./ultrasonic.test');
 
-const normalize = require('./helpers/normalize.test');
-
 describe('Mock', () => {
   describe('cameraMount', cameraMount);
   describe('compass', compass);
   describe('rotary', rotary);
   describe('servo', servo);
   describe('ultrasonic', ultrasonic);
-
-  describe('helpers', () => {
-    describe('normalize', normalize);
-  });
 });
