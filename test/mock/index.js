78100f9112ddbf29a6d2714fed746f7cb04eb1d1
Martijn Vegter <martijn@martijnvegter.com>
2018-11-22 16:18:35 +0100

    I'm hungry

--- test/mock/index.js
+++ test/mock/index.js
@@ -4,10 +4,17 @@ const rotary = require('./rotary.test');
 const servo = require('./servo.test');
 const ultrasonic = require('./ultrasonic.test');
 
+/* Lib */
+const Auxilio = require('./../../lib/Auxilio/test');
+
 describe('Mock', () => {
   describe('cameraMount', cameraMount);
   describe('compass', compass);
   describe('rotary', rotary);
   describe('servo', servo);
   describe('ultrasonic', ultrasonic);
+
+  describe('lib', () => {
+    describe('Auxilio', Auxilio);
+  });
 });
