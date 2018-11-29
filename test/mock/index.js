59d0a0cee846d93cdb6f380d6bce5ee9d0de40e9
Martijn Vegter <martijn@martijnvegter.com>
2018-11-29 11:15:21 +0100

    One does not simply merge into master. (#52)

--- test/mock/index.js
+++ test/mock/index.js
@@ -1,5 +1,4 @@
 const cameraMount = require('./cameraMount.test');
-const compass = require('./compass.test');
 const rotary = require('./rotary.test');
 const servo = require('./servo.test');
 const ultrasonic = require('./ultrasonic.test');
@@ -9,7 +8,6 @@ const Auxilio = require('./../../lib/Auxilio/test');
 
 describe('Mock', () => {
   describe('cameraMount', cameraMount);
-  describe('compass', compass);
   describe('rotary', rotary);
   describe('servo', servo);
   describe('ultrasonic', ultrasonic);
